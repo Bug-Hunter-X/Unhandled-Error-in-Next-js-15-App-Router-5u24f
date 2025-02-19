c73cbae4ce8db86317503c3d5cf1af27d28a0dd1
BugHunter-X <askzenai@gmail.com>
2025-02-20 00:38:05 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,23 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href="/about">
+        <a>Go to About page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error in Next.js 15 if you don't handle it correctly
+  throw new Error('Intentional error for demonstration');
+}
+```
